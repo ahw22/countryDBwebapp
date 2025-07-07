d1a918ed538f67c8cf4b4d024b58b5bc135b4f1b
ahwz <andreas.zincke@gmx.at>
2025-07-07 11:28:16 +0200

    remove data.sql

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO continent (id, name, short_name) VALUES (1, 'Europe', 'EU');
-INSERT INTO country (id, name, domain, area_km2, inhabitants, continent_id)
-VALUES (1, 'Austria', 'AT', 83879, 9027999, 1);
