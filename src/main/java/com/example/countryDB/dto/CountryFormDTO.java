025d05419c853d9ca5309d02db052cb8814abd81
ahwz <andreas.zincke@gmx.at>
2025-07-07 11:32:42 +0200

    implement CountryFormDTO

---
+++ src/main/java/com/example/countryDB/dto/CountryFormDTO.java
@@ -0,0 +1,11 @@
+package com.example.countryDB.dto;
+
+public class CountryFormDTO {
+    private int id;
+    private String name;
+    private String domain;
+    private int areaKm2;
+    private int inhabitants;
+    private int continentId;
+    private int capitalId;
+}
