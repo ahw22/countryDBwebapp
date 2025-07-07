02167dae700d139c42defb2d659b50c3d6cd1942
ahwz <andreas.zincke@gmx.at>
2025-07-07 11:41:48 +0200

    add methods with joins

--- src/main/java/com/example/countryDB/repository/CountryRepository.java
+++ src/main/java/com/example/countryDB/repository/CountryRepository.java
@@ -3,7 +3,23 @@ package com.example.countryDB.repository;
 import com.example.countryDB.model.Capital;
 import com.example.countryDB.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
     Country findByName(String name);
+
+    @Query("SELECT c FROM Country c " +
+            "JOIN FETCH c.continent " +
+            "JOIN FETCH c.capital")
+    List<Country> findAllWithJoins();
+
+    @Query("SELECT c FROM Country c " +
+            "JOIN FETCH c.continent " +
+            "JOIN FETCH c.capital " +
+            "WHERE c.id = :id")
+    Optional<Country> findByIdWithJoins(@Param("id") int id);
 }
